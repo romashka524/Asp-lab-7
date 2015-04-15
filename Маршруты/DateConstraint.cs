b000686140043913c705c483015cbfc96d02dafa
Ushakova Nadezhda <romashe4_ka93@mail.ru>
2015-04-15 19:17:04 +0500

    7

---
+++ Маршруты/DateConstraint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Маршруты
+{
+    public class DateConstraint
+    {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            DateTime d;
+            return DateTime.TryParse(values[parameterName].ToString(), out d);
+        }
+    }
+}
